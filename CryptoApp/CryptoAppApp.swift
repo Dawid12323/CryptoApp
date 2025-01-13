6a7e30b93b47e3e28bf83bb0063069c341381c63
Dawid12323 <krakowski.dawid12@gmail.com>
2025-01-13 11:54:24 +0100

    code created

--- CryptoApp/CryptoAppApp.swift
+++ CryptoApp/CryptoAppApp.swift
@@ -5,7 +5,7 @@ import SimpleSwiftCrypto  // Dodaj import zależności
 struct CryptoApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()  // Używamy ContentView jako głównego widoku
+            ContentView()  //  Używamy ContentView jako głównego widoku
         }
     }
 }
